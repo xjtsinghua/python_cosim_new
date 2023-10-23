4e098d22342f6b783886f88545a56ab9381a46d1
Jake <1378462921@qq.com>
2023-10-23 21:11:51 +0800

    梳理模型代码结构： 1.cosim类:与lasvsim联合仿真调取API接口数据 2.surr类:车辆的属性和基本方法 实现功能： 1.ccbf修改参数和代码，融合clf和cbf输出的控制量 2.new入口文件添加单元测试代码，可以对评估函数输出值进行测试 3.ccbf控制器代码添加单元测试代码，可以对ccbf输出值进行单元测试

--- new.py
+++ new.py
@@ -349,7 +349,7 @@ class Cosim():
                     if not self.vehicle_id_list_new:#todo 找最近10辆控制车
                         ctrl_vehs = []
                         ctrl_l_list,ctrl_f_list = get_ctrl_vehs(all_vehs_pos)
-                        self.vehicle_id_list_new = self.vehicle_id_list + ctrl_l_list
+                        self.vehicle_id_list_new = self.vehicle_id_list #+ ctrl_l_list
                         for veh in self.vehicle_id_list_new:
                             vehicle = stub.GetVehicle(simulation_pb2.GetVehicleReq(simulation_id=self.simulation_id, vehicle_id=veh), metadata=self.metadata)
                             veh_info=dict(Length=vehicle.vehicle.info.base_info.Length,Width=vehicle.vehicle.info.base_info.Width,Weight=vehicle.vehicle.info.base_info.Weight,
